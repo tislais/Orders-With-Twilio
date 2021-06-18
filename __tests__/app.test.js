16c87ae24bb886ae6e3d616a553bbe5ea29afd8c
Tis Lais <tistististis@gmail.com>
2021-06-18 11:12:33 -0700

    fix app breaking path and env issues

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,25 @@
+import dotenv from 'dotenv';
 import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+dotenv.config();
+
+describe('order routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('creates an order via POST', async () => {
+    const res = await request(app)
+      .post('/api/v1/orders')
+      .send({ 
+        quantity: 5
+      });
+    expect(res.body).toEqual({
+      id: 1,
+      quantity: 5
+    });
   });
 });
